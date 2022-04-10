06f35b75aedcac68badc07610ab2d51ff1947b1c
mmoustafunb <mmoustaf@unb.ca>
2022-04-10 13:21:22 -0300

    .

---
+++ Assets/ExitDoorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoorController : MonoBehaviour
+{
+    private string input;
+    AudioSource audio;
+    public UIControl uic;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        uic.Hide();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && (!GameController.key6))
+        {
+            audio.Play();
+        }
+        else if (other.CompareTag("Player") && GameController.key6)
+        {
+            uic.Show();
+        }
+    }
+    public void ReadInput(string password)
+    {
+        input = password;
+        if(input == "952")
+        {
+            GameController.gameState = GameController.GameState.WIN;
+            
+        }
+    }
+}
---
+++ Assets/UIControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Hide();
+    }
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
